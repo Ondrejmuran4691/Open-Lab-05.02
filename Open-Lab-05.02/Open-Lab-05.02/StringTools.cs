42aedea841449da00d2f449652a3abdfd7567af8
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-12-27 21:52:08 +0100

    removes irelavant characters from the end of string

---
+++ Open-Lab-05.02/Open-Lab-05.02/StringTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Open_Lab_05._02
+{
+    public class StringTools
+    {
+        public string NoYelling(string sentence)
+        {
+            if(sentence.EndsWith("!!"))
+            do
+            {
+                sentence = sentence.Remove(sentence.Length - 1);
+            } while (sentence.EndsWith("!!"));
+            else
+            {
+                if (sentence.EndsWith("??"))
+                    do
+                    {
+                        sentence = sentence.Remove(sentence.Length - 1);
+                    } while (sentence.EndsWith("??"));
+                else
+                {
+                 
+                }
+            }
+            return sentence;
+        }
+    }
+}
